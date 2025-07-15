2eb4b42aeaa6ede64adb2546fbc98ecb91ece877
WilliamSnjy <william.sanjaya50@gmail.com>
2025-07-15 16:01:23 +0700

    adding google analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,6 +23,7 @@ export default function RootLayout({ children }) {
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
+      <GoogleAnalytics gaId="G-W89E7582X6" />
     </html>
   );
 }
